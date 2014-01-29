7ec4390d8e6f4eb3caa357b3e4d415dbeb005eeb
cloud-hot <157724595@163.com>
2014-01-29 22:20:57 +0800

    comment apache2 and fetch master rvm

--- manifests/passenger/apache/ubuntu/pre.pp
+++ manifests/passenger/apache/ubuntu/pre.pp
@@ -1,7 +1,7 @@
 class rvm::passenger::apache::ubuntu::pre {
 
   # Dependencies
-  if ! defined(Package['apache2'])              { package { 'apache2':              ensure => present } }
+#  if ! defined(Package['apache2'])              { package { 'apache2':              ensure => present } }
   if ! defined(Package['build-essential'])      { package { 'build-essential':      ensure => present } }
   if ! defined(Package['apache2-prefork-dev'])  { package { 'apache2-prefork-dev':  ensure => present } }
   if ! defined(Package['libapr1-dev'])          { package { 'libapr1-dev':          ensure => present, alias => 'libapr-dev' } }
--- manifests/system.pp
+++ manifests/system.pp
@@ -1,9 +1,12 @@
-class rvm::system($version='latest') {
+class rvm::system(
+  $version ='latest',
+  $branch  ='master',
+) {
   exec { 'system-rvm':
     path    => '/usr/bin:/usr/sbin:/bin',
     command => "bash -c '/usr/bin/curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer ; \
                 chmod +x /tmp/rvm-installer ; \
-                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${version} ; \
+                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --branch ${branch} --version ${version} ; \
                 rm /tmp/rvm-installer'",
     creates => '/usr/local/rvm/bin/rvm',
     require => [
